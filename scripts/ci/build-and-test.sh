db06261a1ac80c4c380a745bbe1d6c52745bbf5e
zhoucli <zhoucli@paypal.com>
2017-05-08 13:23:17 -0700

    test

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -10,12 +10,21 @@ cd $(dirname $0)/../..
 source scripts/ci/sources/mode.sh
 source scripts/ci/sources/tunnel.sh
 
+# Check if tests can be skipped
+skip_tests=true
+for filename in $(git diff --name-only $TRAVIS_BRANCH...HEAD); do
+  if ! [[ "$filename" =~ .*\.md ]]; then
+    skip_tests=false
+    break
+  fi
+done
+
 start_tunnel
 wait_for_tunnel
 
 if is_lint; then
   $(npm bin)/gulp ci:lint
-elif is_e2e; then
+elif is_e2e && (! $skip_tests); then
   $(npm bin)/gulp ci:e2e
 elif is_aot; then
   $(npm bin)/gulp ci:aot
@@ -23,7 +32,7 @@ elif is_payload; then
   $(npm bin)/gulp ci:payload
 elif is_closure_compiler; then
   ./scripts/closure-compiler/build-devapp-bundle.sh
-else
+else if ! $skip_tests; then
   $(npm bin)/gulp ci:test
 fi
 
