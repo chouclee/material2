5dadb2d7480cce07431ff5fcced8aeea2b2db7e7
zhoucli <zhoucli@paypal.com>
2017-05-21 23:26:16 -0700

    update script

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -10,21 +10,34 @@ cd $(dirname $0)/../..
 source scripts/ci/sources/mode.sh
 source scripts/ci/sources/tunnel.sh
 
+# Get commit diff
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then 
+  echo $TRAVIS_COMMIT_RANGE
+  diff=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
+else
+  diff=$(git diff --name-only $TRAVIS_BRANCH...HEAD)
+fi
+
 # Check if tests can be skipped
 skip_tests=true
-for filename in $(git diff --name-only $TRAVIS_BRANCH...HEAD); do
+for filename in $diff; do
+  echo $filename
   if ! [[ "$filename" =~ .*\.md ]]; then
     skip_tests=false
     break
   fi
 done
 
+if skip_tests && (is_e2e || is_unit); then
+  exit 0;
+fi
+
 start_tunnel
 wait_for_tunnel
 
 if is_lint; then
   $(npm bin)/gulp ci:lint
-elif is_e2e && (! $skip_tests); then
+elif is_e2e; then
   $(npm bin)/gulp ci:e2e
 elif is_aot; then
   $(npm bin)/gulp ci:aot
@@ -32,7 +45,7 @@ elif is_payload; then
   $(npm bin)/gulp ci:payload
 elif is_closure_compiler; then
   ./scripts/closure-compiler/build-devapp-bundle.sh
-elif ! $skip_tests; then
+elif is_unit; then
   $(npm bin)/gulp ci:test
 fi
 
